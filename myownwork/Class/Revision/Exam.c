c7811ef4bd79a962508623d4b0bae8457147212e
Celia <joyihirwecelia@gmail.com>
2026-03-21 12:22:53 +0200

    Code base

--- myownwork/Class/Revision/Exam.c
+++ myownwork/Class/Revision/Exam.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
-int main () {
-	int A[3][3] = {{5, 8, 3}, {7, 2, 9}, {1, 6, 4}};
-	int B[3][3] = {{1, 2, 1}, {3, 0, 4}, {2, 5, 1}};
-	int C[3][3];
-	 for (int i =0; i<3; i++){
-	 	for(int j =0; j<3; j++){
+/*int main () {
+	int A[2][2] = {{5, 6}, {7, 8}};
+	int B[2][3] = {{1, 2}, {3, 4}};
+	int C[2][2];
+	 for (int i =0; i<2; i++){
+	 	for(int j =0; j<2; j++){
 	 		C[i][j] = A[i][j] - B[i][j];
 		 }
 	 }
-	 for (int i =0; i<3; i++){
-	 	for(int j =0; j<3; j++){
+	 for (int i =0; i<2; i++){
+	 	for(int j =0; j<2; j++){
 	 		printf("Matrix A[%d][%d]: %d\n ", i, j, A[i][j]);
 	 		printf("Matrix B[%d][%d]: %d\n ", i, j, B[i][j]);
 	 		printf("Subtraction matrix C[%d][%d]: %d\n", i, j, C[i][j]);
 		 }
 	 }	 
 	 return 0;
+}*/
+
+int coutDigits(int n){
+	if( n >= 0 && n < 10){
+		return 1;
+	}
+	return 1+coutDigits(n/10);
+}
+int main (){
+	int nbr;
+	printf("Enter any number: ");
+	scanf("%d", &nbr);
+	printf("The number of digits of %d is %d: ", nbr, coutDigits(nbr));
+	return 0;
 }
